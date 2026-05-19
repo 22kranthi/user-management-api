c062cf9a15666565f5ce6cc8761624b7d13cfa29
22kranthi <kranthisabavat17@gmail.com>
2026-05-19 23:42:55 +0530

    Add BCryptPasswordEncoder bean for password encryption

--- src/main/java/com/example/usermanagement/UserManagementApiApplication.java
+++ src/main/java/com/example/usermanagement/UserManagementApiApplication.java
@@ -2,12 +2,18 @@ package com.example.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UserManagementApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementApiApplication.class, args);
+    }
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
