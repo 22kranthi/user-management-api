7e37eef747e8e8788538e0897de5bff509c577d1
22kranthi <kranthisabavat17@gmail.com>
2026-05-25 22:21:14 +0530

    add new custom exceptions

---
+++ src/main/java/com/example/usermanagement/exception/EmailNotVerifiedException.java
@@ -0,0 +1,12 @@
+package com.example.usermanagement.exception;
+
+public class EmailNotVerifiedException extends RuntimeException {
+
+    public EmailNotVerifiedException(String message) {
+        super(message);
+    }
+
+    public EmailNotVerifiedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
